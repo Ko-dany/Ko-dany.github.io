2509540cdbb2caa85e87259114951f0ae240d8c1
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2024-10-26 22:11:11 -0400

    Corrected base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Ko-dany.github.io",
+  base: "/Ko-dany.github.io/",
 });
