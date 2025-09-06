89b414cd41731e142975b11291a0f9fa6619824a
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2025-09-06 18:26:38 -0400

    Delete DevOps skill, and add Azure, Apache Hadoop

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -6,27 +6,30 @@ const Skill: React.FC = () => {
       <div className="top_header">
         <h1>Technical Skills</h1>
         <p>Technologies I love to play with.</p>
-
       </div>
       <div className="skills_list">
         <div className="skills_item" style={{ textAlign: "center" }}>
-          <img src="https://img.shields.io/badge/Javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=white" />
-          <img src="https://img.shields.io/badge/Typescript-3178C6?style=for-the-badge&logo=typescript&logoColor=white" />
-          <img src="https://img.shields.io/badge/C%23-006600?style=for-the-badge&logo=C%23&logoColor=white" />
-          <img src="https://img.shields.io/badge/Java-007396?style=for-the-badge&logo=Java&logoColor=white" />
-          <img src="https://img.shields.io/badge/Python-004088?style=for-the-badge&logo=Python&logoColor=white" />
+          {/* Languages */}
+          <img src="https://img.shields.io/badge/C%23-006600?style=flat&logo=C%23&logoColor=white" />
+          <img src="https://img.shields.io/badge/Java-037590?style=flat&logo=openjdk&logoColor=white" />
+          <img src="https://img.shields.io/badge/Javascript-F7DF1E?style=flat&logo=javascript&logoColor=white" />
+          <img src="https://img.shields.io/badge/Typescript-3178C6?style=flat&logo=typescript&logoColor=white" />
+          <img src="https://img.shields.io/badge/Python-004088?style=flat&logo=Python&logoColor=white" />
           <br />
-          <img src="https://img.shields.io/badge/ASP.NET-A9225C?style=for-the-badge&logo=dotnet&logoColor=white" />
-          <img src="https://img.shields.io/badge/React.js-61DAFB?style=for-the-badge&logo=React&logoColor=black" />
-          <img src="https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white" />
-          <img src="https://img.shields.io/badge/Bootstrap-7952B3?style=for-the-badge&logo=bootstrap&logoColor=white" />
-          <img src="https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=Node.js&logoColor=white" />
+          {/* Framework & Library */}
+          <img src="https://img.shields.io/badge/ASP.NET-A9225C?style=flat&logo=dotnet&logoColor=white" />
+          <img src="https://img.shields.io/badge/React.js-61DAFB?style=flat&logo=React&logoColor=black" />
+          <img src="https://img.shields.io/badge/Flask-000000?style=flat&logo=flask&logoColor=white" />
+          <img src="https://img.shields.io/badge/Bootstrap-7952B3?style=flat&logo=bootstrap&logoColor=white" />
+          <img src="https://img.shields.io/badge/Node.js-339933?style=flat&logo=Node.js&logoColor=white" />
+          <img src="https://img.shields.io/badge/Mysql-feca57?style=flat&logo=MySql&logoColor=black" />
           <br />
-          <img src="https://img.shields.io/badge/Mysql-feca57?style=for-the-badge&logo=MySql&logoColor=black" />
-          <img src="https://img.shields.io/badge/DevOps-CB2E6D?style=for-the-badge&logo=azuredevops&logoColor=white" />
-          <img src="https://img.shields.io/badge/Linux-E95420?style=for-the-badge&logo=linux&logoColor=white" />
-          <img src="https://img.shields.io/badge/Jenkins-D24939?style=for-the-badge&logo=Jenkins&logoColor=white" />
-          <img src="https://img.shields.io/badge/Bitbucket-0052CC?style=for-the-badge&logo=Bitbucket&logoColor=white" />
+          {/* Others */}
+          <img src="https://img.shields.io/badge/Azure-008AD7?style=flat&logo=azuredevops&logoColor=white" />
+          <img src="https://img.shields.io/badge/Jenkins-D24939?style=flat&logo=Jenkins&logoColor=white" />
+          <img src="https://img.shields.io/badge/Linux-E95420?style=flat&logo=linux&logoColor=white" />
+          <img src="https://img.shields.io/badge/Selenium-43B02A?style=flat&logo=selenium&logoColor=white" />
+          <img src="https://img.shields.io/badge/Apache Hadoop-66CCFF?style=flat&logo=apachehadoop&logoColor=black" />
         </div>
       </div>
     </section>
