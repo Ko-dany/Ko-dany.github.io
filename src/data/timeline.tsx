5cdc6ba8be5d3d836932f54c80311070811f1ad8
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2025-09-06 18:17:53 -0400

    Add work experiences of OCAS and Acceldata

--- src/data/timeline.tsx
+++ src/data/timeline.tsx
@@ -1,28 +1,39 @@
 export default [
   {
-    title: "DevOps Software Developer",
+    title: "Software Developer",
+    company: "Acceldata",
+    companyUrl: "https://www.acceldata.io/about-us",
+    location: "Kitchener, ON, Canada",
+    duration: "Jun 2025 - Present",
+    description:
+      "I started as a co-op and currently continue part-time. Installed and maintained Apache open-source projects like Ambari and Hadoop, upgrading legacy components (such as Kafka and Airflow) from JDK 11 to JDK 17 to ensure full compatibility with Ambari’s updated Java runtime. Led a large-scale merge of upstream Ambari 3.0.0 into Acceldata’s customized Ambari fork, integrating over 300 commits and resolving complex conflicts.",
+  },
+  {
+    title: "Quality Analyist Co-op",
+    company: "OCAS",
+    companyUrl: "https://www.ocas.ca/what-we-do/ontariocolleges",
+    location: "Guelph, ON, Canada (Remote)",
+    duration: "Jan 2025 - Apr 2025",
+    description:
+      "I automated UI smoke tests for .NET web applications, cutting manual testing time from about 1 hour to just 6 minutes. I integrated these automated tests into Azure DevOps pipelines, ensuring the full process automatically completes in 8 minutes. By designing HTTP status validation and UI verification, I identified 15+ issues across multiple projects before deployment.",
+  },
+  {
+    title: "DevOps Software Developer Co-op",
     company: "VARLab",
     companyUrl:
       "https://www.linkedin.com/company/varlab-virtual-and-augmented-reality-lab/posts/?feedView=all",
     location: "Waterloo, ON, Canada",
-    duration: "Sep 2024 - Present",
+    duration: "Sep 2024 - Dec 2024",
     description:
-      "As part of the DevOps team, I maintained Jenkins pipelines to automate testing, reporting, and deployment for Unity projects. I was involved in designing new pipelines from the ground up for Node.js projects, implementing linting, static code analysis, unit testing and integration testing. Additionally, I established a containerization stage in the Jenkins pipeline using Docker and Azure Container Registry for streamlined depolyment. For our internal Jenkins server, I improved memory usage by 28% (from 242.9GB to 174.8GB) through automated scripting. I also managed Linux-based web servers and integrated them into the LMS platforms, such as Moodle and D2L Brightspace, using the LTI protocol, optimizing deployment and operational workflows for Unity projects.",
+      "I designed and developed Jenkins pipelines to automate testing, reporting, and deployment for Unity and Node.js projects. Containerized web applications using Docker, and integrated them with Azure Container Registry. Also managed web hosting servers and connected them to LMS platforms (D2L, Moodle) via the LTI protocol.",
   },
   {
-    title: "Data Specialist",
+    title: "Data Specialist Co-op",
     company: "I.M.A Ltd.",
     companyUrl: "https://www.imaltd.com/",
     location: "Tillsonburg, ON, Canada (Remote)",
     duration: "May 2024 - Aug 2024",
     description:
-      "I developed a web application using React.js and GraphQL to enhance the UI/UX of internal company web services, and performed data cleansing to improve data accuracy and reliability. Through this experience, I strengthened my communication skills and adaptability by applying customized data cleansing standards tailored to client requirements. I also traveled to Wisconsin, USA, to communicate with clients in person and conduct on-site data collection.",
+      "I standardized client inventory data to ensure accuracy, consistently exceeding the company’s targets. Built an internal React web application with features like permission-based user management, duplicate data checks, and enhanced UI/UX for better user interaction. Also traveled to the U.S. to collaborate with clients on-site and manage a data collection project.",
   },
-  // {
-  //   title: "Title",
-  //   location: "Company",
-  //   duration: "How many years",
-  //   description:
-  //     "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  // },
 ];
