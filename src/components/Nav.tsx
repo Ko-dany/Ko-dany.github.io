3718b4d2a8a3ca23bad7f5f6d160d5a214914677
Dahyun (Dany) <88699158+Ko-dany@users.noreply.github.com>
2025-09-06 23:28:26 -0400

    Fix home button toggle tansition, and add motion dependency for anmitaion

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,37 +1,49 @@
 import { Icon } from "@iconify/react";
 import "../css/nav.css";
 import { useState } from "react";
-import resume from "../../public/assets/Dahyun_Ko_Resume.pdf";
+import resume from "/assets/Dahyun_Ko_Resume.pdf";
+import * as motion from "motion/react-client";
+import { AnimatePresence } from "motion/react";
 
 const Nav: React.FC = () => {
-  const [isMouseOver, SetIsMouseOver] = useState(false);
-  const onMouseOverHandler = () => {
-    SetIsMouseOver((prev) => !prev);
-  };
+  const [isMouseOverHome, SetIsMouseOverHome] = useState(false);
 
   return (
     <nav id="header">
-      <div className="nav_logo">
-        <p className="nav_name">
-          <a
-            href="#intro"
-            className={
-              isMouseOver ? "animate__animated animate__fadeInDown" : ""
-            }
-            onMouseOver={onMouseOverHandler}
-            onMouseOut={onMouseOverHandler}
-          >
-            {isMouseOver ? "고 다현" : "Dany Ko"}
-          </a>
-        </p>
+      <div className="nav_name">
+        <motion.a
+          href="#intro"
+          className={`nav_name_content ${
+            isMouseOverHome ? "animate__animated" : ""
+          }`}
+          onMouseEnter={() => SetIsMouseOverHome(true)}
+          onMouseLeave={() => SetIsMouseOverHome(false)}
+        >
+          <AnimatePresence mode="wait">
+            {isMouseOverHome ? (
+              <motion.span
+                key="name-ko"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                고 다현
+              </motion.span>
+            ) : (
+              <motion.span
+                key="name-en"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                Dany Ko
+              </motion.span>
+            )}
+          </AnimatePresence>
+        </motion.a>
       </div>
       <div className="nav_menu" id="navMenu">
         <ul className="nav_menu_list">
-          <li className="nav_list_item">
-            <a href="#intro" className="nav_link">
-              Home
-            </a>
-          </li>
           <li className="nav_list_item">
             <a href="#about" className="nav_link">
               Experience
